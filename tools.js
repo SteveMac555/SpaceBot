031bb2ec675eaf6675eeb6b46064fd7845009938
Steve <smm@waleshigh.com>
2019-10-24 13:11:45 +0100

    Add files via upload

---
+++ tools.js
@@ -0,0 +1,50 @@
+module.exports = {
+    startup: function() {
+        const figlet = require("figlet");
+        console.log('\033[2J');
+        figlet('SPACEBOT', function(err, data) {
+        if (err) {
+            console.log('Something went wrong...', err);
+        } else {
+            console.log(data);
+        }
+        });   
+    },
+    d_console: function (msg, c) {
+        var prepend = '';
+        switch(c) {
+          case 'y':
+            prepend = '\x1b[33m';
+          break;
+          case 'c':
+            prepend = '\x1b[36m';
+          break;
+          case 'g':
+            prepend = '\x1b[32m';
+          break;
+          case 'r':
+            prepend = '\x1b[31m';
+          break;
+          case 'm':
+            prepend = '\x1b[35m';
+          break;
+          default:
+            prepend = '\x1b[37m';
+          break;
+        }
+        console.log (prepend, "[" + new Date().toLocaleString() + "] > " + msg);
+    },
+    getSubTier: function (subPlan) {
+        if (subPlan == 1000) {
+            return "Tier 1";
+        } else if (subPlan == 2000) {
+            return "Tier 2";
+        } else if (subPlan == 3000) {
+            return "Tier 3";
+        } else if (subPlan == "Prime") {
+            return "Prime";
+        } else {
+            return "Unknown";
+        }
+    }
+};
