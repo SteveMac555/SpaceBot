ba63fd3b6868652e328d8a8ee613d14b0cdd1398
Steve <smm@waleshigh.com>
2019-10-24 13:11:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,28 @@
 const tmi = require('tmi.js');
 const _ = require('underscore');
 const https = require('https');
-const figlet = require("figlet");
+const fs = require('fs');
+const tools = require('./tools');
 
-const client = new tmi.client({
-  identity: {
-    username: "<USERNAME>",
-    password: "oauth:<YOURTOKEN>"
-  },
-  connection: {
-    reconnect: true,
-  },
-  channels: [
-    "<YOUR CHANNEL>",
-  ]
-});
+var settings = JSON.parse(fs.readFileSync('settings.json'));
+const client = new tmi.client(settings);
 
 const cmds = {
     prefix: '!',
     delay_default: 500,
     commands: {
         ping: {
-            responder: 'ping',
-            calls: 'onPing',
-            permission: ['broadcaster', 'moderator', 'subscriber', 'viewer']
+          responder: 'ping',
+          calls: 'onPing',
+          permission: ['broadcaster', 'moderator'],
+          user_allow: ['stevemac555'],
+          delay: 600
         },
         rand: {
-            responder: 'rand',
-            calls: 'onRand',
-            permission: ['broadcaster'],
+          responder: 'rand',
+          calls: 'onRand',
+          permission: ['broadcaster'],
+          delay:1000,
         },
         spacex: {
           responder: 'spacex',
@@ -39,50 +33,53 @@ const cmds = {
     }
 }
 
+tools.startup();
 client.connect();
-console.log('\033[2J');
-figlet('SPACEBOT', function(err, data) {
-  if (err) {
-      console.log('Something went wrong...', err);
-  }else{
-      console.log(data);
-  }
-}); 
 
 client.on("connecting", (address, port) => {
-  d_console(`Attempting to connect to ${address}:${port}`, 'y');
+  tools.d_console(`Attempting to connect to ${address}:${port}`, 'y');
 });
 
 client.on("connected", (address, port) => {
-  d_console(`Connected to: ${address}:${port}`, 'y');
+  tools.d_console(`Connected to: ${address}:${port}`, 'y');
 });
 
 client.on("logon", () => {
-  d_console(`Connection established: Logging In...`, 'y');
+  tools.d_console(`Connection established: Logging In...`, 'y');
 });
 
 client.on("mods", (channel, mods) => {
-  d_console(`Moderators of ${channel} are ${mods}`, 'g');
+  tools.d_console(`Moderators of ${channel} are ${mods}`, 'g');
 });
 
 client.on("raided", (channel, username, viewers) => {
-  d_console(`Channel ${channel} is being raided by ${username} with ${viewers} viewers.`, 'c');
+  tools.d_console(`Channel ${channel} is being raided by ${username} with ${viewers} viewers.`, 'c');
 });
 
 client.on("message", (channel, userstate, msg, self) => {
   if (self) return;
+
+  switch(userstate["message-type"]) {
+    case "chat":
+      tools.d_console(`[CHAT] ${userstate.username}: ${msg}`, 'm');
+    break;
+    case "whisper":
+      tools.d_console(`[WHISPER] ${userstate.username}: ${msg}`, 'm');
+    break;
+}
+
   if (msg.trim().charAt(0) == cmds.prefix) {
     var params = msg.trim().split(' ');
     var userPermission = (userstate.badges.broadcaster ? `broadcaster` : (userstate.mod ? `moderator` : (userstate.subscriber ? `subscriber` : `viewer`)));
     _.each(cmds.commands, function (v, k) {
       if ((cmds.prefix + v.responder) == params[0]) {
-        if (v.permission.indexOf(userPermission) > -1) {
+        if (v.permission.indexOf(userPermission) > -1 || v.user_allow.indexOf(userstate.username) > -1) {
           setTimeout(function() {
             eval(v.calls)(channel, userstate, params);
           }, (v.delay ? v.delay : cmds.delay_default));
-          d_console(`Processed command: ${cmds.prefix}${v.responder} from ${userstate.username}. User Permission: ${userPermission}`, 'c');
+          tools.d_console(`Processed command: ${cmds.prefix}${v.responder} from ${userstate.username}.`, 'c');
         } else {
-          d_console(`Denied command: ${cmds.prefix}${v.responder} from ${userstate.username}. User Permission: ${userPermission} Required Permission: ${v.permission}`, 'r')
+          tools.d_console(`Denied command: ${cmds.prefix}${v.responder} from ${userstate.username}. User Permission: ${userPermission} Required Permission: ${v.permission}`, 'r')
         }
       }
     });
@@ -90,17 +87,39 @@ client.on("message", (channel, userstate, msg, self) => {
 });
 
 client.on("roomstate", (channel, state) => {
-  d_console(`Joined channel: ${channel}`, 'g');
+  tools.d_console(`Joined channel: ${channel}`, 'g');
   client.say(channel, "/mods", 'y');
 });
 
 client.on("disconnected", (reason) => {
-  d_console(`Disconnected from server, reason: ${reason}`, 'r');
+  tools.d_console(`Disconnected from server, reason: ${reason}`, 'r');
 });
 
 function onPing(channel, userstate, params) {
     client.say(channel, `@${userstate.username} PONG!`);
 }
+
+fs.watchFile('settings.json', (curr, prev) => {
+  const newSettings = JSON.parse(fs.readFileSync('settings.json'));
+  var newChannels = newSettings.channels;
+  var oldChannels = settings.channels;
+
+  _.each(newChannels, function (newChannel) {
+    if (oldChannels.indexOf(newChannel) == -1) {
+      tools.d_console("Channel Added. Joining: " + newChannel, 'g');
+      client.join(newChannel);
+    }
+  });
+
+  _.each(oldChannels, function (oldChannel) {
+    if (newChannels.indexOf(oldChannel) == -1) {
+      tools.d_console("Channel Removed. Leaving: " + oldChannel, 'g');
+      client.part(oldChannel);
+    }
+  });
+
+  settings = JSON.parse(fs.readFileSync('settings.json'));
+});
 
 function onRand(channel, userstate, params) {
   client.say(channel, `@${userstate.username} You rolled a ${ Math.round(Math.random() * ((isNaN(params[1]) ? 100 : params[1]) - 1) + 1)}`);
@@ -117,26 +136,4 @@ function onSpaceX(channel, userstate, params) {
   }).on('error', function(e){
         client.say(channel, "Unable to contact SpaceX API.");
   });
-}
-
-function d_console(msg, c) {
-  var prepend = '';
-  switch(c) {
-    case 'y':
-      prepend = '\x1b[33m';
-    break;
-    case 'c':
-      prepend = '\x1b[36m';
-    break;
-    case 'g':
-      prepend = '\x1b[32m';
-    break;
-    case 'r':
-      prepend = '\x1b[31m';
-    break;
-    default:
-      prepend = '\x1b[37m';
-    break;
-  }
-  console.log (prepend, "[" + new Date().toLocaleString() + "] > " + msg);
 }
